075c8d3d25992bfe4a2dbec84d8bcd0e65857c69
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-05-18 13:47:05 -0300

    Add project files.

---
+++ MethodAssignment/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Class1
+    {
+        public int intquintuple(int x)
+        {
+            return x * 5;
+        }
+        public int intsextuple(int x)
+        {
+            return x * 6;
+
+        }
+        public int intseptuple(int x)
+        {
+            return x * 7;
+        }
+    }
+}
---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number: 10, 20, 30, 40, 50, 60");
+            int userInp = Convert.ToInt32(Console.ReadLine());
+
+            Class1 obj1 = new Class1();
+
+            Console.WriteLine("Your number quintupled is " + obj1.intquintuple(userInp));
+            Console.WriteLine("Your number sextupled is " + obj1.intsextuple(userInp));
+            Console.WriteLine("Your number septupled is " + obj1.intseptuple(userInp));
+            Console.ReadLine();
+
+        }
+    }
+}
